01dc4f58775a7b07e054f4904ec72739129f424d
Hogan, Shahn <shogan@midwayusa.com>
2022-11-28 09:00:43 -0600

    Add File > Exit menu

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,21 @@
     windows_subsystem = "windows"
 )]
 
+use tauri::{CustomMenuItem, Menu, Submenu};
+
 fn main() {
+    let exit = CustomMenuItem::new("exit".to_string(), "Exit");
+    let submenu = Submenu::new("File", Menu::new().add_item(exit));
+    let menu = Menu::new().add_submenu(submenu);
+
     tauri::Builder::default()
+        .menu(menu)
+        .on_menu_event(|event| match event.menu_item_id() {
+            "exit" => {
+                std::process::exit(0);
+            }
+            _ => {}
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
